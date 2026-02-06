50d9279d01c1975c44dad3a73dbe6fae9d9c8883
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-02-06 23:53:46 +0530

    practice

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log(`Hello world!`);
+console.log(`This is a simple Node.js application.`);
+console.log(`You can run this application using 'node index.js' command.`);
+console.log(`Feel free to modify this code and experiment with Node.js!`);
